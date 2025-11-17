a4484e6ae23b94d63b661488d1adb0283199ef9b
boyAndo <iva.duarte@duocuc.cl>
2025-11-16 23:58:33 -0300

    Update import statement for PrismaClient in refresh-token.service

--- src/services/shared/refresh-token.service.ts
+++ src/services/shared/refresh-token.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/index.js";
+import { PrismaClient } from "../../generated/prisma";
 import jwt from "jsonwebtoken";
 import crypto from "node:crypto";
 
